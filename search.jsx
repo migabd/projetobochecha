e25b184cfcaa24c738c0946b0e2d651bde4bd176
migabd <151981183+migabd@users.noreply.github.com>
2026-05-15 20:06:12 -0300

    Delete search.jsx

--- search.jsx
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('c:/Users/gabri/OneDrive/Documentos/Caderno/index.html', 'utf8').split('\n');
-lines.forEach((line, index) => {
-    if (line.includes('elaboradorState') || line.includes('Elaborador')) {
-        console.log(`${index + 1}: ${line.trim()}`);
-    }
-});
